61ed87119c8b3a53618c0423677fa9e991fc5f8c
JiamingHao <hao1@email.arizona.ed>
2019-09-21 22:10:35 -0400

    Re-arrange the code.

--- timed_out_and_back.py
+++ timed_out_and_back.py
@@ -38,130 +38,86 @@ class OutAndBack():
         # How fast will we update the robot's movement?
         self.rate = 50
 
-        # Used to compare with the sign of distance to determine direction
-	self.preDir = 1.0
-        self.preAng = 1.0
-
         # Set the equivalent ROS rate variable
         self.r = rospy.Rate(self.rate)
 
         # Set the forward linear speed to 0.2 meters per second
         self.linear_speed = 0.2
 
-        # Set the travel distance to 1.0 meters
-        # goal_distance = 1.0
-
-        # How long should it take us to get there?
-        # linear_duration = goal_distance / linear_speed
-
         # Set the rotation speed to 1.0 radians per second
         self.angular_speed = 1.0
 
+        nan_msg = "Not a number!"
+        ukn_msg = "Unknown operation, please enter T, R or Q!"
+        while True:
+            user_input = raw_input("Enter T to translate, R to rotate, or Q to quit: ")
 
-    def Translations(self, dis):
-        print "Will perform a translation for " + dis
-	
-	if (float(dis) / abs(float(dis))) != self.preDir:
- 			self.linear_speed = - self.linear_speed
+            if user_input == 'T':
+                dist_str = raw_input("Enter a distance (in meters, negative numbers for reversing): ")
+                if is_number(dist_str):
+                    self.translations(dist_str)
+                else:
+                    print nan_msg
 
-        linear_duration = abs(float(dis)) / abs(self.linear_speed)
+            elif user_input == 'R':
+                ang_str = raw_input("Enter a angle (in degrees, positive being counterclockwise): ")
+                if is_number(ang_str):
+                    self.rotations(ang_str)
+                else:
+                    print nan_msg
 
+            elif user_input == 'Q':
+                break
+
+            else:
+		try:
+                   print ukn_msg
+		except KeyboardInterrupt:
+		   print "Interrupted!"
+
+
+    def translations(self, dist_str):
+        distance = float(dist_str)
+        self.linear_speed = abs(self.linear_speed) * (distance / abs(distance))
+        linear_duration = abs(distance / self.linear_speed)
+
+        # Start translation
         move_cmd = Twist()
         move_cmd.linear.x = self.linear_speed
-        ticks = int( linear_duration * self.rate)
+        ticks = int(linear_duration * self.rate)
         for t in range(ticks):
-        	self.cmd_vel.publish(move_cmd)
-            	self.r.sleep()
+            self.cmd_vel.publish(move_cmd)
+            self.r.sleep()
+
+        # Stop robot
         move_cmd = Twist()
         self.cmd_vel.publish(move_cmd)
-
-	self.preDir = float(dis) / abs(float(dis))
         rospy.sleep(1)
-    '''
-	if float(dis) * self.preDir >= 0:
-		print "Positive translations!"
-        	linear_duration = float(dis) / self.linear_speed
-        	move_cmd = Twist()
-        	move_cmd.linear.x = self.linear_speed
-        	ticks = int( linear_duration * self.rate)
-        	for t in range(ticks):
-        		self.cmd_vel.publish(move_cmd)
-                	self.r.sleep()
-        	move_cmd = Twist()
-        	self.cmd_vel.publish(move_cmd)
-        	rospy.sleep(1)
-	else:
-		print "Negative Distance!"
-		move_cmd = Twist()
-		angular_duration = pi / self.angular_speed
 
-                move_cmd.angular.z = self.angular_speed
-                ticks = int(angular_duration * self.rate)
-                for t in range(ticks):
-                	self.cmd_vel.publish(move_cmd)
-              		self.r.sleep()
+    def rotations(self, ang_str):
+        ang_rad = math.radians(float(ang_str))
+        self.angular_speed = abs(self.angular_speed) * (ang_rad / abs(ang_rad))
+        angular_duration = abs(ang_rad / self.angular_speed)
 
-		move_cmd = Twist()
-        	self.cmd_vel.publish(move_cmd)
-        	rospy.sleep(1)
+        # Start rotation
+        move_cmd = Twist()
+        move_cmd.angular.z = self.angular_speed
+        ticks = int(angular_duration * self.rate)
+        for t in range(ticks):
+            self.cmd_vel.publish(move_cmd)
+            self.r.sleep()
 
-        	linear_duration = abs(float(dis)) / self.linear_speed
-        	move_cmd = Twist()
-        	move_cmd.linear.x = self.linear_speed
-        	ticks = int( linear_duration * self.rate)
-        	for t in range(ticks):
-        		self.cmd_vel.publish(move_cmd)
-                	self.r.sleep()
-        	move_cmd = Twist()
-        	self.cmd_vel.publish(move_cmd)
-        	rospy.sleep(1)
-    '''
-
-    def Rotations(self, ang):
-	print "Will perform rotation for: " + ang + " degree"
-	rad = math.radians(float(ang))
-
-	if rad >= 0:
-		print "Turn counterclockwise for radians " + str(abs(rad))
-
-		move_cmd = Twist()
-                move_cmd.angular.z = self.angular_speed
-		angular_duration = abs(rad) / self.angular_speed
-
-		ticks = int(angular_duration * self.rate)
-                for t in range(ticks):
-                	self.cmd_vel.publish(move_cmd)
-              		self.r.sleep()
-
-		move_cmd = Twist()
-        	self.cmd_vel.publish(move_cmd)
-        	rospy.sleep(1)
-	else:
-		print "Turn clockwise for radians for radians " + str(abs(rad))
-
-		move_cmd = Twist()
-                move_cmd.angular.z = - self.angular_speed
-		angular_duration = abs(rad) / self.angular_speed
-
-                ticks = int(angular_duration * self.rate)
-                for t in range(ticks):
-                	self.cmd_vel.publish(move_cmd)
-              		self.r.sleep()
-
-		move_cmd = Twist()
-        	self.cmd_vel.publish(move_cmd)
-        	rospy.sleep(1)
-
-
-
-
-
+        # Stop robot
+        move_cmd = Twist()
+        self.cmd_vel.publish(move_cmd)
+        rospy.sleep(1)
 
     def shutdown(self):
         # Always stop the robot when shutting down the node.
         rospy.loginfo("Stopping the robot...")
         self.cmd_vel.publish(Twist())
         rospy.sleep(1)
+
 
 def is_number(s):
     try:
@@ -170,40 +126,9 @@ def is_number(s):
     except ValueError:
         return False
 
+
 if __name__ == '__main__':
-
-    print("Trying   -- Ma Hao")
-    robot = OutAndBack()
-    while True:
-        user_input = raw_input("Enter a T for translation, R for rotation, or Q for quit: ")
-
-        if user_input == 'T':
-        	print "Translations!"
-        	dis = raw_input("Enter a distance for the translations: ")
-                if is_number(dis):
-        		print "Is a number!"
-                        #robot = OutAndBack()
-        		robot.Translations(dis)
-                else:
-        		print "Not a number!"
-
-        elif user_input == 'R':
-        	print "Rotations!"
-        	ang = raw_input("Enter a degree for the rotation: ")
-                if is_number(ang):
-        		print "Is a number!"
-                        #robot = OutAndBack()
-        		robot.Rotations(ang)
-                else:
-        		print "Not a number!"
-
-        elif user_input == 'Q':
-    	    print "Quit!"
-            break
-        else:
-            print "Unknown"
-
-    '''try:
+    try:
         OutAndBack()
     except:
-        rospy.loginfo("Out-and-Back node terminated.")'''
+        rospy.loginfo("Out-and-Back node terminated.")
